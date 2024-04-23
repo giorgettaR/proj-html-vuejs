b5c9a99b5c1a0a58f128b14ee7a243f078016267
GiorgettaR <giorgetta.roberto@gmail.com>
2024-04-23 16:19:43 +0200

    ADD: section projects

--- src/components/SectionProjects.vue
+++ src/components/SectionProjects.vue
@@ -2,7 +2,37 @@
 </script>
 
 <template>
-
+<section class="projects block">
+    <div class="container">
+        <div class="row pb-3">
+            <div class="head col-7 m-auto d-flex flex-column align-items-center justify-content-between">
+                <h4>Explore Recent Work</h4>
+                <hr class="divider small">
+                <p class="text-center">Quis autem vel eum iure repredeirt qui in eavoluptate velit esse quam nihil molestiae conssequatur.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 m-auto">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="project first"></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="project second"></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="project third"></div>
+                    </div>
+                </div>
+                <div class="row divider big">
+                    <hr class="col-4 left">
+                    <p class="col-4 text-center">View All Projects</p>
+                    <hr class="col-4 right">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </template>
 
 <style lang="scss">
